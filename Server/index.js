af5a53d5be82ef1e2e1030bd42d5834edd592b55
Tom AF <tom.effraf@gmail.com>
2025-06-05 11:22:43 +0100

    step: fiddling with spotify auth

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import router from './routes/SRroutes.js'
+import router from './routes/SRroutes.js';
+import cors from 'cors';
+require('dotenv').config();
 
+//standard spin up
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
-
 
 //connected to router
 app.use('/', router)
